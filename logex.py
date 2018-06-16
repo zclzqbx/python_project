4e67a2cc7fb20602f3f54cd3a2955b13650ba97d
zclzqbx@163.com <zclzqbx@163.com>
2018-06-16 08:11:51 -0700

    logex update

---
+++ logex.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding=utf8 -*-
+"""
+# Author: zhuchuanlin
+# Created Time : Sat 16 Jun 2018 07:31:16 AM PDT
+# File Name: logex.py
+# Description:
+"""
+
+
+import logging
+import os
+import sys
+
+class Log:
+    def __init__(self, module='defautl_module', path='/home/walker/Log/', level=logging.DEBUG):
+         # 创建一个logger
+        self.logger = logging.getLogger(module)
+        self.logger.setLevel(logging.DEBUG)
+
+        is_dir_exist = os.path.exists(path)
+        if not is_dir_exist:
+            os.makedirs(path, 777)
+
+        filename = path + ("%s.log"%(module))
+
+        # 创建一个handler，用于写入日志文件
+        fh = logging.FileHandler(filename)
+        fh.setLevel(logging.DEBUG)
+
+         # 再创建一个handler，用于输出到控制台
+        ch = logging.StreamHandler(sys.stdout)
+        ch.setLevel(logging.DEBUG)
+
+        # 定义handler的输出格式
+        formatter = logging.Formatter('%(asctime)s %(filename)s:%(lineno)s %(levelname)s : %(message)s')
+        fh.setFormatter(formatter)
+        ch.setFormatter(formatter)
+
+        # 给logger添加handler
+        self.logger.addHandler(fh)
+        self.logger.addHandler(ch)
+
+
+        self.write_log_fatal = self.logger.fatal
+        self.write_log_error = self.logger.error
+        self.write_log_warning = self.logger.warning
+        self.write_log_info = self.logger.info
+        self.write_log_debug = self.logger.debug
+
+
+if __name__ == '__main__':
+    logger = Log()
+    logger.write_log_fatal("live or death")
---
+++ observe.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding=utf8 -*-
+"""
+# Author: zhuchuanlin
+# Created Time : Fri 08 Jun 2018 08:10:55 AM PDT
+# File Name: observe.py
+# Description:
+"""
+
+import urllib
+
+
