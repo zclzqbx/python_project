ed56f357e14d9c6596fec7ff495abec50156b1b0
zclzqbx@163.com <zclzqbx@163.com>
2018-06-18 05:16:42 -0700

    error log

--- logex.py
+++ logex.py
@@ -18,15 +18,24 @@ class Log:
         self.logger = logging.getLogger(module)
         self.logger.setLevel(level)
 
-        is_dir_exist = os.path.exists(path)
+        err_log_path = path + 'Error_Log/'
+
+        is_dir_exist = os.path.exists(err_log_path)
+
+        #is_dir_exist = os.path.exists(path)
         if not is_dir_exist:
-            os.makedirs(path, 777)
+            os.makedirs(err_log_path, 777)
 
         filename = path + ("%s.log"%(module))
 
         # 创建一个handler，用于写入日志文件
         fh = logging.FileHandler(filename)
         fh.setLevel(logging.DEBUG)
+
+        filename = err_log_path + ("%s_err.log"%(module))
+
+        fh_err = logging.FileHandler(filename)
+        fh_err.setLevel(logging.ERROR)
 
          # 再创建一个handler，用于输出到控制台
         ch = logging.StreamHandler(sys.stdout)
@@ -36,10 +45,12 @@ class Log:
         formatter = logging.Formatter('%(asctime)s %(filename)s:%(lineno)s %(levelname)s : %(message)s')
         fh.setFormatter(formatter)
         ch.setFormatter(formatter)
+        fh_err.setFormatter(formatter)
 
         # 给logger添加handler
         self.logger.addHandler(fh)
         self.logger.addHandler(ch)
+        self.logger.addHandler(fh_err)
 
 
         self.write_log_fatal = self.logger.fatal
@@ -52,3 +63,4 @@ class Log:
 if __name__ == '__main__':
     logger = Log()
     logger.write_log_fatal("live or death")
+    logger.write_log_warning("try to live long")
